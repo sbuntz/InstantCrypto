5bd32112420d7c62e0c30f911c692d83229904e4
josephdaw <josephdaw@icloud.com>
2021-10-14 19:27:05 +1030

    basic outline for data display

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// function to display coin information
+function displayCoinData(){
+    // variables for HTML elements
+    const coinContainer = $('coin-container');
+    const coinHeading = $('coin-heading');
+    const coinPrice = $('coin-price');
+
+    
+
+    // print data to the page
+    coinHeading.text(coinName)
+    coinPrice.text(coinPrice)
+};
+
+// function to display news information
+function displayCoinData(){
+    const newsContainer = $('news-container');
+
+    
+};
