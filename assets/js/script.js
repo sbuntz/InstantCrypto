ebe2c2e213983632c54bbd6e7a0929a6419742d7
josephdaw <josephdaw@icloud.com>
2021-10-18 22:23:32 +1030

    append news results to news-container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// element variables
+const newContainerEL = $('#news-container')
 
 $("#search-button").on("click", function (event) {
     // stop the form submitting
@@ -20,12 +22,18 @@ function searchCoin(coinID) {
             // append data to the page
             $("#coin-heading").text(data[0].name);
             $("#coin-price").text(`$ ${data[0].current_price}`);
+            $("#coin-heading").text(data[0].name);
+            $("#coin-price").text(`$ ${data[0].current_price}`);
+            $("#coin-price").text(`$ ${data[0].price_change_percentage_24h}`);
+            $("#coin-price").text(`$ ${data[0].ath}`);
+            $("#coin-price").text(`$ ${data[0].high_24h}`);
+            $("#coin-price").text(`$ ${data[0].low_24h}`);
         })
 };
 
 
 function newsCall(coinID) {
-    console.log("newsCoin");
+
     const queryURL = `https://newsapi.org/v2/top-headlines?q=${coinID}&language=en&category=business&apiKey=822c6daf68da47f2aa999e05473aa7bb`;
 
     fetch(queryURL)
@@ -35,15 +43,45 @@ function newsCall(coinID) {
         .then(function (data) {
             // process the data before appending
             processData(data);
+
         })
 };
 
 function processData(data) {
 
-    for (var i = 0; i < 5; i++) {
-        var author = data.articles[i].author;
+    for (var i = 0; i < 4; i++) {
+        // var author = data.articles[i].author;
+        var author = data.articles[i].source.name;
+  
         var title = data.articles[i].title;
-        var artUrl = data.articles[i].url;
+        var articleUrl = data.articles[i].url;
+        var imgUrl = data.articles[i].urlToImage;
+
         // append goes here
+        const tileEL = $('<div class="tile is-parent">');
+        const articleEL = $('<article class="tile is-child box">');
+        const titleEl = $('<h2 class="subtitle">');
+        const authorEl = $('<p class="subtitle">');
+        const linkEl = $('<a>');
+        const imageEl = $('<img src="" alt="">');
+
+
+        // display date, description, and icon
+        titleEl.text(title)
+        authorEl.text(author);
+        imageEl.attr('src', imgUrl)
+        linkEl.attr('href', articleUrl)
+        
+        // set link to open in new tab
+        linkEl.attr('target', '_blank')
+
+        // put image inside link
+        linkEl.append(imageEl)
+
+        // add elements to the page
+        articleEL.append(titleEl, authorEl, linkEl);
+        tileEL.append(articleEL);
+        newContainerEL.append(tileEL);
+
     };
 }
