94e7d6a24c4dd234c030f5ce71fbab65b39a3290
Stefanie Buntz <stbuntz@gmail.com>
2021-10-20 16:11:50 +1030

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+function limit (string = '', limit = 0) {  
+    return string.substring(0, limit)
+  }
+  
 
 
 function searchCoin(coinID) {
@@ -47,63 +51,31 @@ function searchCoin(coinID) {
         })
 };
 
-
+//http://api.mediastack.com/v1/news?access_key=a626b109f9191a2796d483deac47f740&categories=business&countries=au,us
 function newsCall(coinID) {
 
-    const queryURL = `https://newsapi.org/v2/top-headlines?q=${coinID}&language=en&category=business&apiKey=822c6daf68da47f2aa999e05473aa7bb`;
+    const queryURL = `http://api.mediastack.com/v1/news?access_key=a626b109f9191a2796d483deac47f740&keywords=${coinID}&categories=business&countries=au,us`;
 
     fetch(queryURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            // process the data before appending
-            processData(data);
-
+           //     console.log(data)
+                for(var i = 0; i < 6; i++) {
+                    $("#news-title_"+i).text(data.data[i].title);
+                    $("#news-author_"+i).text(data.data[i].author);                  
+                    $("#news-url_"+i).attr('href', data.data[i].url);
+                    $("#news-image_"+i).attr('src', data.data[i].image); 
+                    $("#news-country_"+i).text(data.data[i].country);
+                    $("#news-description_"+i).text(data.data[i].description);     
+                
+                
+                  }
         })
 };
 
-function processData(data) {
 
-    // clear previous news articles
-    $('#news-container').empty()
-
-    for (var i = 0; i < 4; i++) {
-        // var author = data.articles[i].author;
-        var author = data.articles[i].source.name;
-
-        var title = data.articles[i].title;
-        var articleUrl = data.articles[i].url;
-        var imgUrl = data.articles[i].urlToImage;
-
-        // append goes here
-        const tileEL = $('<div class="tile is-parent">');
-        const articleEL = $('<article class="tile is-child box">');
-        const titleEl = $('<h2 class="subtitle">');
-        const authorEl = $('<p class="subtitle">');
-        const linkEl = $('<a>');
-        const imageEl = $('<img src="" alt="">');
-
-
-        // display date, description, and icon
-        titleEl.text(title)
-        authorEl.text(author);
-        imageEl.attr('src', imgUrl)
-        linkEl.attr('href', articleUrl)
-
-        // set link to open in new tab
-        linkEl.attr('target', '_blank')
-
-        // put image inside link
-        linkEl.append(imageEl)
-
-        // add elements to the page
-        articleEL.append(titleEl, authorEl, linkEl);
-        tileEL.append(articleEL);
-        newContainerEL.append(tileEL);
-
-    };
-}
 
 function saveLastSearch(searchHistory){
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
@@ -132,3 +104,8 @@ $(document).ready(function () {
         searchCoin(coinID)
     });
 })
+
+
+
+
+
